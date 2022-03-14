cdf7bb8768ac5eb133eead2672b3f878925ccec3
kalistalks <62800848+kalistalks@users.noreply.github.com>
2022-03-14 16:42:00 +0800

    Update conditionals_if_else.cpp

--- Practice Solutions/conditionals_if_else.cpp
+++ Practice Solutions/conditionals_if_else.cpp
@@ -1,1 +1,28 @@
-// here
+// conditionals first practice 
+int main(){
+  int x; 
+  cin >> x; 
+  if (x % 2 == 0){
+    cout << "Even";
+  }
+  else {
+    cout << "Odd";
+  }
+}
+
+// conditionals second practice
+int main(){
+    int score;
+    cin >> score;
+    if ((score >= 90) && (score <= 100)) {
+        cout << "A*";
+    } else if ((score >= 70) && (score <= 89)) {
+        cout << "A";
+    } else if ((score >= 60) && (score <= 69)) {
+        cout << "B";
+    } else if ((score >= 50) && (score <= 59)) {
+        cout << "C";
+    } else {
+        cout << "F";
+    }
+}
