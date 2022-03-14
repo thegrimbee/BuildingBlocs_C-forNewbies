d42dc07c23110551611744e720b459d08b272253
kalistalks <62800848+kalistalks@users.noreply.github.com>
2022-03-14 16:50:22 +0800

    Update loops.cpp

--- Practice Solutions/loops.cpp
+++ Practice Solutions/loops.cpp
@@ -1,1 +1,22 @@
-// here
+#include <bits/stdc++.h>
+using namespace std;
+
+// while solution
+int main(){
+  int counter = 1;
+  int total = 0;
+  while (counter <= 10){
+    total += counter;
+    counter ++;
+  }
+  cout << total << endl;
+}
+
+// for solution
+int main(){
+  int total = 0; 
+  for (int counter = 0; counter <= 10; counter++){
+    total += counter;
+  }
+  cout << total << endl;
+}
